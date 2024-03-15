ddb511e680b29666813b04c40d930c00c9b10935
shomil03 <shomil02020@gmail.com>
2024-03-16 01:58:26 +0530

    made home screen and checkout screen

---
+++ CupcakeCorner/HomeScreen.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CupcakeCorner
+//
+//  Created by Shomil Singh on 12/03/24.
+//
+
+import SwiftUI
+
+struct HomeScreen: View {
+   
+    
+    var body: some View {
+        VStack{}
+    
+    }
+}
+
+#Preview {
+    HomeScreen()
+}
